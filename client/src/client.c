450c4818f088a36a7a7b493d8498732885403628
lfiorenzo <leofiorenzo9@gmail.com>
2024-04-20 20:26:15 +0000

    logger

--- client/src/client.c
+++ client/src/client.c
@@ -19,6 +19,8 @@ int main(void)
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
+	log_info (logger, "Hola! Soy un log");
+
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -55,6 +57,8 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
+
+	nuevo_logger = log_create ("tp0.log", "LOGGERTP0", 1, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -97,4 +101,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	  log_destroy (logger);
 }
